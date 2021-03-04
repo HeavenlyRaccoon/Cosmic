d477e3c6da49fd51dda18897368cdd832e8d0fa5
HeavenlyRaccoon <vova55565@gmail.com>
2021-03-04 22:18:09 +0300

    Тестовый консольный проект

---
+++ Tests/CosmicConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CosmicConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
