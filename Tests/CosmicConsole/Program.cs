da4f806852e14deef7a9f21e59e72fb85e385134
HeavenlyRaccoon <vova55565@gmail.com>
2021-03-09 14:50:55 +0300

    Тест Спотифай Апи Поиск музыки

--- Tests/CosmicConsole/Program.cs
+++ Tests/CosmicConsole/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
-
+using System.Threading.Tasks;
+using SpotifyAPI.Web;
 namespace CosmicConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Prof();
+            Task.WaitAll(Prof());
+        }
+
+        public static async void Mus()
+        {
+            
+        }
+
+        public static async Task Prof()
+        {
+            var config = SpotifyClientConfig.CreateDefault();
+
+            var request = new ClientCredentialsRequest("e38f5549120e4f7885ed1472dc58adb6", "0e255d6f56d9433da52bfab4e8e3cf4b");
+            var response = new OAuthClient(config).RequestToken(request);
+
+           var spotify = new SpotifyClient(config.WithToken(response.Result.AccessToken));
+            var song = await spotify.Search.Item(new SearchRequest(SearchRequest.Types.Track, "Талия"));
+            await foreach(var i in spotify.Paginate(song.Tracks, (s) => s.Tracks)){
+                Console.WriteLine(i.Name);
+            }
+            
+
+            //Console.WriteLine(song);
         }
     }
 }
