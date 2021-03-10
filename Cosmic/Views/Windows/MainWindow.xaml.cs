e887dcf390c9e83334bc9221c0cff1ff7685f4e0
HeavenlyRaccoon <vova55565@gmail.com>
2021-03-10 19:31:28 +0300

    Доделан дизайн средних блоков: Популярная музыка, Новинки музыки. Сделан подвал приложения

--- Cosmic/Views/Windows/MainWindow.xaml.cs
+++ Cosmic/Views/Windows/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,11 @@ namespace Cosmic
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://vk.com/id210553771");
         }
     }
 }
